a5efbf289a7a882ef461dafb7d80f78355a87429
Shobhit Agarwal <shobhitagarwal2003@gmail.com>
2024-06-02 13:22:01 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const {PORT}=require("./config/serverConfig");
-const {sendBasicEmail}=require("./services/email-service");
+// const {sendBasicEmail}=require("./services/email-service");
+const jobs=require("./utils/jobs");
+const TicketController=require("./controller/ticket-controller");
 
 const setupAndStartServer=()=>{
     const app=express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    app.post("/api/v1/tickets",TicketController.create);
 
     app.listen(PORT,()=>{
         console.log(`Server started at port ${PORT}`);
-        sendBasicEmail(
-        "support@admin.com",
-        "somnathagarwal696@gmail.com",
-        "This is a testing and system generated email",
-        "Hey, how are you? I hope you are doing fine in life"
-        );
+        jobs();
     });
-
+    
 }
 setupAndStartServer();
+
+
+// sendBasicEmail(
+// "support@admin.com",
+// "somnathagarwal696@gmail.com",
+// "This is a testing and system generated email",
+// "Hey, how are you? I hope you are doing fine in life"
+// );
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -2,6 +2,8 @@ const sender=require("../config/emailConfig");
 // sender is the transport object used to send the emails
 // In case of gmail as a mailing service *from* key will always have
 // email id as the one set up on SMTP Server even if different email id was passed as argument
+const TicketRepository=require("../repository/ticket-repository");
+const repo= new TicketRepository();
 
 const sendBasicEmail=async (mailFrom,mailTo,mailSubject,mailBody)=>{
     try {
@@ -11,12 +13,42 @@ const sendBasicEmail=async (mailFrom,mailTo,mailSubject,mailBody)=>{
             subject:mailSubject,
             text:mailBody
         }); 
-        console.log( response);
+        // console.log( response);
     } catch (error) {
      console.log(error);   
     }
 }
 
+const fetchPendingEmails=async ()=>{
+    try {
+        const response= await repo.get({status:"PENDING"});
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updateTicket=async (ticketId,data)=>{
+    try {
+        const response= await repo.update(ticketId,data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const createNotification=async(data)=>{
+    try {
+       const ticket= await repo.create(data);
+       return ticket;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports={
-    sendBasicEmail
+    sendBasicEmail,
+    fetchPendingEmails,
+    createNotification,
+    updateTicket
 }
